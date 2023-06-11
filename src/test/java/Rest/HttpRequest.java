28490f213bcb32265cd7733fd59e174fc363f1f8
Brumaxonics <chumaxonics@gmail.com>
2023-06-11 22:02:15 +0200

    API Testing

--- src/test/java/Rest/HttpRequest.java
+++ src/test/java/Rest/HttpRequest.java
@@ -10,6 +10,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 
 
+import java.util.HashMap;
+
 import static io.restassured.RestAssured.given;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matcher.*;
@@ -29,6 +31,23 @@ WebDriver driver;
 
                 .then()
                 .statusCode(200)
+                .log().all();
+    }
+
+
+    @Test
+    public void createUser(){
+        HashMap  data = new HashMap<>();
+        data.put("name","Nyiko");
+        data.put("Job","Automation Engineer");
+
+        given()
+                .contentType("application/json")
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .statusCode(201)
                 .log().all();
     }
 
