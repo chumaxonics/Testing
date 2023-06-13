617a2253fa4ccee72d90978479a49b30dea9f6a6
Brumaxonics <chumaxonics@gmail.com>
2023-06-13 08:57:56 +0200

    API Testing | RestAssured

--- src/test/java/Rest/Requests.java
+++ src/test/java/Rest/Requests.java
@@ -1,5 +1,13 @@
 package Rest;
 
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+
+import static io.restassured.RestAssured.given;
+
 public class Requests {
 
     /*
@@ -9,4 +17,24 @@ public class Requests {
     - using POJO (plain Old Java Object
     - Using external Json file
      */
+    @Test(priority = 1)
+    public void createUserAndGetResponseIdTest() {
+        HashMap data = new HashMap<>();
+        data.put("name", "Nyiko");
+        data.put("Job", "Automation Engineer");
+
+      Response response= given()
+                .contentType("application/json")
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+              .statusCode(200)
+              .log().body()
+              .extract().response();
+
+      String jsonSting = response.asString();
+        Assert.assertEquals(jsonSting.contains("Ned-bank Happened"),true);
+    }
+
 }
